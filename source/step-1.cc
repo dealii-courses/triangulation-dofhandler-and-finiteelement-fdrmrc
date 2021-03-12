d854515ff68dab6546999768a2f45a3c3133c063
Marco Feder <marco.feder96@gmail.com>
2021-03-12 01:44:55 +0100

    refining L-shaped domain

--- source/step-1.cc
+++ source/step-1.cc
@@ -38,10 +38,10 @@ first_grid()
   GridGenerator::hyper_cube(triangulation);
   triangulation.refine_global(4);
 
-  std::ofstream out("grid-1.svg");
+  std::ofstream out("grid-1.vtu");
   GridOut       grid_out;
   grid_out.write_svg(triangulation, out);
-  std::cout << "Grid written to grid-1.svg" << std::endl;
+  std::cout << "Grid written to grid-1.vtu" << std::endl;
 }
 
 
@@ -86,9 +86,65 @@ second_grid()
 
 
 
+void
+third_grid()
+{
+  Triangulation<2> triangulation;
+  const double     left  = -1.0;
+  const double     right = +1.0;
+  GridGenerator::hyper_L(triangulation, left, right, true);
+  std::ofstream out("grid-3.svg");
+  GridOut       grid_out;
+
+  grid_out.write_svg(triangulation, out);
+  std::cout << "Grid written to grid-3.svg" << std::endl;
+
+  std::cout << "Now refine the grid globally" << std::endl;
+  const unsigned int initial_global_refinement = 2;
+
+  triangulation.refine_global(initial_global_refinement);
+  std::ofstream out_refined("grid-4.svg");
+
+  grid_out.write_svg(triangulation, out_refined);
+  std::cout << "Grid written to grid-4.svg" << std::endl;
+
+
+  std::cout << "Refine adaptively around the re-entrant corner"
+            << "\n";
+  const Point<2>     corner(0.0, 0.0);
+  const unsigned int no_refinements{6};
+  for (unsigned int step = 0; step < no_refinements; ++step)
+    {
+      for (auto &cell : triangulation.active_cell_iterators())
+        {
+          for (const auto v : cell->vertex_indices())
+            {
+              const double distance_from_corner =
+                corner.distance(cell->vertex(v));
+
+              if (std::fabs(distance_from_corner) <= (1.0) / (3.0))
+                {
+                  cell->set_refine_flag();
+                  break;
+                }
+            }
+        }
+    }
+  triangulation.execute_coarsening_and_refinement();
+
+
+  std::ofstream out_refined_locally("grid-5.svg");
+
+  grid_out.write_svg(triangulation, out_refined_locally);
+  std::cout << "Grid written to grid-4.svg" << std::endl;
+}
+
+
+
 int
 main()
 {
   first_grid();
   second_grid();
+  third_grid();
 }
